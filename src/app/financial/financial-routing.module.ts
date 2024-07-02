ba1dbfc0b6df0432a00bc49636793e2d131564b0
jonato96 <jonato96s@gmail.com>
2024-07-01 19:52:52 -0500

    add-project-routes

--- src/app/financial/financial-routing.module.ts
+++ src/app/financial/financial-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListProductComponent } from './pages/list-product/list-product.component';
+import { CreateProductComponent } from './pages/create-product/create-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list-product',
+    component: ListProductComponent
+  },
+  {
+    path: 'create-product',
+    component: CreateProductComponent
+  },
+  {
+    path: 'edit-product',
+    component: CreateProductComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'list-product'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
