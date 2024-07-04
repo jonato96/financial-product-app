7d18f049a9674f9e7936ab7b1a4a1c55e0a969ee
jonato96 <jonato96s@gmail.com>
2024-07-04 08:50:10 -0500

    add-validators

--- src/app/financial/pages/create-product/create-product.component.ts
+++ src/app/financial/pages/create-product/create-product.component.ts
@@ -19,8 +19,29 @@ export class CreateProductComponent {
     revisionDate: []
   });
 
+  isValidField(field: string): boolean | null {
+    return this.productForm.controls[field].errors && this.productForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if(!this.productForm.controls[field]) return null;
+
+    const errors = this.productForm.controls[field].errors || {};
+
+    for(const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required': return `Field ${field} is required!`;
+      }
+    }
+    return null;
+  }
+
   save(){
     
+  }
+
+  reset() {
+    this.productForm.reset();
   }
 
 }
