87defd4ce5914503c0457f904e28e1b01c4ea185
jonato96 <jonato96s@gmail.com>
2024-07-05 19:43:21 -0500

    add-logo-click-redirect

--- src/app/financial/components/banner/banner.component.ts
+++ src/app/financial/components/banner/banner.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
+
+  private readonly router = inject(Router);
+
+  redirect() {
+    this.router.navigate(['financial/list-product']);
+  }
 
 }
